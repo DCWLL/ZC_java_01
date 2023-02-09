34049de6321cb87a0368c4e80ba51ae228e2ea2a
Zhongqi Chen <chenzhongqi002@gmail.com>
2023-02-09 16:23:58 -0700

    T

--- B16SC/src/Autobox_cast.java
+++ B16SC/src/Autobox_cast.java
@@ -16,19 +16,12 @@ public class Autobox_cast {
         Integer age_2 = age_1; // (auto) box int to Integer
         int age_3 = age_2;     // (auto) unbox Integer to int
 
-
         // type cast, these two are the same
         double age_4 = age_1;               // auto
         double age_5 = (double) age_1;      // manually
         System.out.println(age_4);
 
-
-
     }
-
-
-
-
 
 }
 
